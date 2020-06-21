1961575753273fc23e0ae38510d8a65064765f98
Jose Cueva Celis <cuevacelis@hotmail.com>
2020-06-21 16:25:28 -0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:best_flutter_ui_templates/principal/app_theme.dart';
+import 'package:flutter/material.dart';
+import 'package:best_flutter_ui_templates/principal/navigation_home_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter T3',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        textTheme: AppTheme.textTheme,
+        platform: TargetPlatform.iOS,
+      ),
+      home: NavigationHomeScreen(),
+    );
+  }
+}
+
+class HexColor extends Color {
+  HexColor(final String hexColor) : super(_getColorFromHex(hexColor));
+  static int _getColorFromHex(String hexColor) {
+    hexColor = hexColor.toUpperCase().replaceAll('#', '');
+    if (hexColor.length == 6) {
+      hexColor = 'FF' + hexColor;
+    }
+    return int.parse(hexColor, radix: 16);
+  }
+}
+
+/*
+--Reglas aplicadas de Material Design:
+  C1:Login(1)
+    Sí
+  C2:MaterialDesign(8)
+    1)Boton Flotante
+    2)Iconos
+    3)Menu de Opciones
+    4)MaterialStateColor
+    5)FadeTransition
+    6)BoxShadow 
+    7)Canvas
+  C3:Navegabilidad(8)
+  C4:Interfaces(6)
+  C5:Widgets(8)
+*/
---
+++ lib/principal/navigation_home_screen.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:best_flutter_ui_templates/principal/app_theme.dart';
+//Home
+import 'package:best_flutter_ui_templates/custom_drawer/drawer_user_controller.dart';
+import 'package:best_flutter_ui_templates/custom_drawer/home_drawer.dart';
+//Navigation
+import 'package:best_flutter_ui_templates/principal/home_screen.dart';
+import 'package:best_flutter_ui_templates/principal/help_screen.dart';
+import 'package:best_flutter_ui_templates/principal/feedback_screen.dart';
+import 'package:best_flutter_ui_templates/principal/invite_friend_screen.dart';
+
+class NavigationHomeScreen extends StatefulWidget {
+  @override
+  _NavigationHomeScreenState createState() => _NavigationHomeScreenState();
+}
+
+class _NavigationHomeScreenState extends State<NavigationHomeScreen> {
+  Widget screenView;
+  DrawerIndex drawerIndex;
+
+  @override
+  void initState() {
+    drawerIndex = DrawerIndex.HOME;
+    screenView = const MyHomePage();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: AppTheme.nearlyWhite,
+      child: SafeArea(
+        top: false,
+        bottom: false,
+        child: Scaffold(
+          backgroundColor: AppTheme.nearlyWhite,
+          body: DrawerUserController(
+            screenIndex: drawerIndex,
+            drawerWidth: MediaQuery.of(context).size.width * 0.75,
+            onDrawerCall: (DrawerIndex drawerIndexdata) {
+              changeIndex(drawerIndexdata);
+              //devolución de llamada desde el cajón para reemplazar la pantalla según lo requerido el usuario al pasar DrawerIndex (índice Enum)
+            },
+            screenView: screenView,
+            //Reemplazamos la vista de pantalla como necesitamos en las pantallas de inicio de navegación como Mi página de inicio, Pantalla de ayuda, Pantalla de comentarios, etc.
+          ),
+        ),
+      ),
+    );
+  }
+
+  void changeIndex(DrawerIndex drawerIndexdata) {
+    if (drawerIndex != drawerIndexdata) {
+      drawerIndex = drawerIndexdata;
+      if (drawerIndex == DrawerIndex.HOME) {
+        setState(() {
+          screenView = const MyHomePage();
+        });
+      } else if (drawerIndex == DrawerIndex.Help) {
+        setState(() {
+          screenView = HelpScreen();
+        });
+      } else if (drawerIndex == DrawerIndex.FeedBack) {
+        setState(() {
+          screenView = FeedbackScreen();
+        });
+      } else if (drawerIndex == DrawerIndex.Invite) {
+        setState(() {
+          screenView = InviteFriend();
+        });
+      } else {
+        //para mas contenido :V
+      }
+    }
+  }
+}
